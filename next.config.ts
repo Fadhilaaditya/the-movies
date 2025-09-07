4bde9c66a8fccf83ddacf45a0252708a437925b1
Fadhilaaditya <fadhilaaditya@gmail.com>
2025-09-07 15:40:31 +0700

    new update

--- next.config.ts
+++ next.config.ts
@@ -9,16 +9,6 @@ const nextConfig = {
     // ❗️Abaikan error ESLint saat build
     ignoreDuringBuilds: true,
   },
-  async redirects() {
-    return [
-      { source: '/search', destination: '/Search', permanent: true },
-      { source: '/movie/:id', destination: '/Movie/:id', permanent: true },
-      { source: '/tv/:id', destination: '/Tv/:id', permanent: true },
-      { source: '/watch/:id', destination: '/Watch/:id', permanent: true },
-      // ejaan: arahkan yang benar ke yang ada saat ini (atau ubah folder nanti)
-      { source: '/release-year', destination: '/realease-year', permanent: true },
-    ];
-  },
 };
 
 module.exports = nextConfig;
