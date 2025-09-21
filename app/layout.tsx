d424a4d69a560473eca2b068f92812a1329d937b
Fadhilaaditya <fadhilaaditya@gmail.com>
2025-09-21 21:42:48 +0700

    update analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/layout/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
@@ -21,9 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${plusJakartaSans.variable} antialiased bg-zinc-950`}>
       <Navbar />
+      <body className={`${plusJakartaSans.variable} antialiased bg-zinc-950`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
